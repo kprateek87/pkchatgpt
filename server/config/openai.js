c5d8a67e863c5a198e660fa0e78f0226441adc20
Prateek <92998340+kprateek87@users.noreply.github.com>
2025-10-17 02:57:26 +0000

    openai

--- server/config/openai.js
+++ server/config/openai.js
@@ -1,5 +1,6 @@
+import { config } from "dotenv";
 import { OpenAI } from "openai";
-
+config();
 const openai = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
   baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
